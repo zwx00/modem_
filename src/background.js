7042a2d4bf531d71aa718e28d5f284160e1d3837
zwx00 <bblokar@gmail.com>
2019-12-08 18:00:56 +0100

    introduce painter

--- src/background.js
+++ src/background.js
@@ -2,93 +2,72 @@ import * as PIXI from 'pixi.js';
 import Utils from './utils';
 const ticker = PIXI.Ticker.shared;
 
-const randomDirection = () => {
-  return Math.random() > 0.5 ? -1 : 1;
-};
+class SpritePainter {
+  constructor (sprite) {
+    this.surfaceWidth = window.innerWidth;
+    this.surfaceHeight = window.innerHeight;
 
-const getSprite = (resource, meta) => {
-  if (meta.type === 'static') {
-    return new PIXI.Sprite(resource.texture);
-  } else if (meta.type === 'animation') {
-    const textures = [];
-    for (let i = 0; i < meta.frames; i++) {
-      textures.push(new PIXI.Texture(resource.texture, new PIXI.Rectangle(
-        i * meta.offset, 0, meta.offset, resource.texture.height
-      )));
-    }
+    const ratio = (this.surfaceHeight / sprite.height / 5) * Math.random();
 
-    const sprite = new PIXI.AnimatedSprite(textures);
-    sprite.gotoAndPlay(0);
-    sprite.animationSpeed = 0.1;
-    return sprite;
-  } else {
-    throw new Error('Unknown filetype');
+    this.sprite = sprite;
+
+    this.sprite.width = sprite.width * ratio + 50;
+    this.sprite.height = sprite.height * ratio + 50;
+
+    this.sprite.x = Math.random() * this.surfaceWidth - sprite.width / 2;
+    this.sprite.y = Math.random() * this.surfaceHeight - sprite.height / 2;
+
+    this.spriteData = {
+      xChange: Math.random() * Utils.randomDirection() * 0.000005,
+      yChange: Math.random() * Utils.randomDirection() * 0.000005,
+      rotationSpeed: Math.random() * 0.00000002
+    };
   }
-};
 
-const paintMovingSprite = ({ meta, resource, container }) => {
-  const surfaceWidth = window.innerWidth;
-  const surfaceHeight = window.innerHeight;
+  updateSprite (delta) {
+    this.sprite.rotation += delta * this.spriteData.rotationSpeed;
 
-  let spriteData = {};
-  const sprite = getSprite(resource, meta);
-
-  const ratio = (surfaceHeight / sprite.height / 5) * Math.random();
-  sprite.width = sprite.width * ratio + 50;
-  sprite.height = sprite.height * ratio + 50;
-
-  sprite.x = Math.random() * surfaceWidth - sprite.width / 2;
-  sprite.y = Math.random() * surfaceHeight - sprite.height / 2;
-
-  spriteData = {
-    xChange: Math.random() * randomDirection() * 0.000005,
-    yChange: Math.random() * randomDirection() * 0.000005,
-    rotationSpeed: Math.random() * 0.00000002,
-    sprite: sprite
-  };
-
-  ticker.add((delta) => {
-    spriteData.sprite.rotation += delta * spriteData.rotationSpeed;
-
-    if (spriteData.sprite.x + delta * spriteData.xChange > surfaceWidth) {
-      spriteData.xChange = -1 * spriteData.xChange;
+    if (this.sprite.x + delta * this.spriteData.xChange > this.surfaceWidth) {
+      this.spriteData.xChange = -1 * this.spriteData.xChange;
     }
-    if (spriteData.sprite.y + delta * spriteData.xChange > surfaceHeight) {
-      spriteData.yChange = -1 * spriteData.yChange;
+    if (this.sprite.y + delta * this.spriteData.xChange > this.surfaceHeight) {
+      this.spriteData.yChange = -1 * this.spriteData.yChange;
     }
 
-    if (spriteData.sprite.x + delta * spriteData.xChange < 0) {
-      spriteData.xChange = -1 * spriteData.xChange;
+    if (this.sprite.x + delta * this.spriteData.xChange < 0) {
+      this.spriteData.xChange = -1 * this.spriteData.xChange;
     }
-    if (spriteData.sprite.y + delta * spriteData.xChange < 0) {
-      spriteData.yChange = -1 * spriteData.yChange;
+    if (this.sprite.y + delta * this.spriteData.xChange < 0) {
+      this.spriteData.yChange = -1 * this.spriteData.yChange;
     }
 
-    spriteData.sprite.x += delta * spriteData.xChange;
-    spriteData.sprite.y += delta * spriteData.yChange;
-  });
+    this.sprite.x += delta * this.spriteData.xChange;
+    this.sprite.y += delta * this.spriteData.yChange;
+  }
+}
 
-  return sprite;
+const paintMovingSprite = ({ meta, resource }) => {
+  const sprite = Utils.getSprite(resource, meta);
+
+  const currentSpritePainter = new SpritePainter(sprite);
+
+  ticker.add(currentSpritePainter.updateSprite.bind(currentSpritePainter));
+
+  return currentSpritePainter.sprite;
 };
 
 const renderBackground = ({ container, fileNames }) => {
   return fileNames.map((object, index) => {
-    return Utils.sleep(2000 * index)
+    Utils.sleep(2000 * index)
       .then(() => {
-        return new Promise(resolve => {
-          new PIXI.Loader()
-            .add(object.filename)
-            .load((_, resources) =>
-              resolve(paintMovingSprite({ meta: object, resource: resources[object.filename]}))
-            );
-        });
+        new PIXI.Loader()
+          .add(object.filename)
+          .load((_, resources) => {
+            const sprite = paintMovingSprite({ meta: object, resource: resources[object.filename] });
+            container.addChild(sprite);
+          });
       });
-  }).map(
-    (task) => {
-      task.then(element => {
-        container.addChild(element);
-      });
-    });
+  });
 };
 
 export { renderBackground };
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,30 @@
+import * as PIXI from 'pixi.js';
+
 export default {
   sleep (t) {
     return new Promise(resolve => {
       setTimeout(resolve, t);
     });
+  },
+  getSprite (resource, meta) {
+    if (meta.type === 'static') {
+      return new PIXI.Sprite(resource.texture);
+    } else if (meta.type === 'animation') {
+      const textures = [];
+      for (let i = 0; i < meta.frames; i++) {
+        textures.push(new PIXI.Texture(resource.texture, new PIXI.Rectangle(
+          i * meta.offset, 0, meta.offset, resource.texture.height
+        )));
+      }
+      const sprite = new PIXI.AnimatedSprite(textures);
+      sprite.gotoAndPlay(0);
+      sprite.animationSpeed = 0.1;
+      return sprite;
+    } else {
+      throw new Error('Unknown filetype');
+    }
+  },
+  randomDirection () {
+    return Math.random() > 0.5 ? -1 : 1;
   }
 };
