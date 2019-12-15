6de06dc51938f9b4a53859b758d6b15f00d22074
zwx00 <bblokar@gmail.com>
2019-12-15 21:53:03 +0100

    fix css to make it fullscreen

--- src/index.js
+++ src/index.js
@@ -17,13 +17,17 @@ const app = new PIXI.Application({
 
 document.body.appendChild(app.view);
 
+/* css for fullscreen */
+document.body.style.overflow = 'hidden';
+document.body.style.padding = 0;
+document.body.style.margin = 0;
+
 /* config */
 app.renderer.backgroundColor = 0xA9A9A9;
 app.stage.sortableChildren = true;
 
 const renderContainers = {
-  menu: null,
-  background: null
+  menu: null
 };
 
 const getAssets = (assetsData, mix, layer) => {
@@ -57,8 +61,6 @@ const renderPage = () => {
   for (const layer of layers) {
     const container = new PIXI.Container();
     const files = getAssets(AssetData, mixName, layer.name);
-    console.log(layer);
-    console.log(layer.renderer);
     layer.renderer.bind(layer)(files, container);
     app.stage.addChild(container);
   }
