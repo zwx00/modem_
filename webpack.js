0fa642a6ee126b0fa4b46aa69b16d7432fd626f7
zwx00 <bblokar@gmail.com>
2019-12-25 18:49:13 +0100

    enable bundler

---
+++ webpack.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  resolve: {
+    alias: {
+      Strategies: path.resolve(__dirname, 'src/strategies/'),
+      '~': path.resolve(__dirname, 'src/')
+    }
+  },
+  output: {
+    filename: 'main.js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              ['@babel/plugin-transform-runtime',
+                {
+                  regenerator: true
+                }
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'modem_ website'
+    }),
+    new CopyPlugin([
+      { from: 'src/assets', to: 'assets' }
+    ])
+  ]
+};
