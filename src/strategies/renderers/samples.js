cf7f14479d863ae6e66183ec72e92023daee0589
blaz.blokar <blaz.blokar@bitstamp.net>
2020-11-01 20:59:41 +0100

    fix for filter

--- src/strategies/renderers/samples.js
+++ src/strategies/renderers/samples.js
@@ -25,7 +25,6 @@ const samplesLayerRenderer = async function (fileNames, container, rootStage) {
   let painted = [];
   while (true) {
     await Utils.sleep(Math.random() * 5000);
-
     const selectedFile = randomElement(fileNames, painted);
 
     if (selectedFile) {
@@ -33,12 +32,11 @@ const samplesLayerRenderer = async function (fileNames, container, rootStage) {
       painted.push(selectedFile.filename);
     }
 
-    if (container.children > 13) {
-      const randomChild = randomElement(container.children);
+    if (container.children.length > 13) {
+      const randomChild = container.children[Math.floor(Math.random() * container.children.length)]
 
       container.removeChild(randomChild);
-
-      painted = painted.filter(file => randomChild._filename !== file);
+      painted = painted.filter(file => decodeURIComponent(new URL(randomChild._filename).pathname) !== `/${file}`)
     }
   }
 };
